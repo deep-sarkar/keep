70cc86bf62a0381f93451f8a4d9fba3979c2f7dd
deep-sarkar <deepsarkar96@gmail.com>
2020-10-06 21:31:42 +0530

    [Deep]Add.added custom status and codes for that

---
+++ fundoo/status.py
@@ -0,0 +1,30 @@
+
+response_code = {
+    200 : "Success",
+    201 : "Created",
+    202 : "Updated",
+    203 : "Ok",
+    300 : "Validation error",
+    301 : "Error occoured while sending activation email",
+    302 : "User account is altrady activated",
+    303 : "Email user does not exists",
+    304 : "Token Expired",
+    305 : "Error occour while redirecting",
+    306 : "Insufficient data",
+    401 : "Username already exists",
+    403 : "Password didnt matched",
+    404 : "Provide valid email",  
+    405 : "Invalid data",
+    406 : "Please enter alpha numeric, atleast 8 digit password",
+    407 : "Token does not exists",
+    408 : "Email already exists in registered user",
+    409 : "Does not exists",
+    410 : "Logout first",
+    411 : "Verify email first",
+    412 : "Wrong password",
+    413 : "Login first",
+    414 : "Invalid operation",
+    415 : "Enter upcoming time",
+    416 : "Something went wrong. Pleaase Try again"
+    
+}
