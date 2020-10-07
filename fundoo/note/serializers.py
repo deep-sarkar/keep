3b38b57b9ebe64d8e6e766c2a3ce7e318fce54cd
deep-sarkar <deepsarkar96@gmail.com>
2020-10-07 16:06:22 +0530

    [Deep]Add.added update method in serializer

--- fundoo/note/serializers.py
+++ fundoo/note/serializers.py
@@ -12,3 +12,14 @@ class EditNoteSerializer(serializers.ModelSerializer):
         model = Note
         fields = "__all__"
         read_only_fields = ['id', 'user']
+
+    def update(self, instance, validated_data):
+        instance.title     = validated_data.get("title", instance.title)
+        instance.note      = validated_data.get("note", instance.note)
+        instance.image     = validated_data.get("image", instance.image)
+        instance.reminder  = validated_data.get("reminder", instance.reminder)
+        instance.archive   = validated_data.get("archive", instance.archive)
+        instance.trash     = validated_data.get("trash", instance.trash)
+        instance.pin       = validated_data.get("pin", instance.pin)
+        instance.save()
+        return instance
