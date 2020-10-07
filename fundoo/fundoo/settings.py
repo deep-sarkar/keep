6f0475d7d98374a50a50aaf6ea6f8872a1667d44
deep-sarkar <deepsarkar96@gmail.com>
2020-10-07 11:56:09 +0530

    [Deep]Add.added new app settings in setting

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'account',
     'django_short_url',
+    'note'
 ]
 
 MIDDLEWARE = [
@@ -126,12 +127,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER='wotbsingh@gmail.com' 
 EMAIL_HOST_PASSWORD='9950618403'
+
+LOGIN_URL = '/account/login/'
+
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
