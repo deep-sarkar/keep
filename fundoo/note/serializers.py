c884342442f62f448c4b81098b836e2d560b8cf6
deep-sarkar <deepsarkar96@gmail.com>
2020-10-08 21:38:08 +0530

    [Deep]Add.added GetNoteSerializer

--- fundoo/note/serializers.py
+++ fundoo/note/serializers.py
@@ -1,16 +1,29 @@
 from rest_framework import serializers
-from note.models import Note
+from note.models import Note, Label
+
+# class LabelSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model  = Label
+#         fields = '__all__'
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Note
-        fields = "__all__"
+        model            = Note
+        fields           = "__all__"
+        read_only_fields = ['id', 'user', 'trash']
+
+class GetNoteSerializer(serializers.ModelSerializer):
+    labels = serializers.StringRelatedField(many=True)
+    class Meta:
+        model            = Note
+        fields           = "__all__"
         read_only_fields = ['id', 'user', 'trash']
 
 class EditNoteSerializer(serializers.ModelSerializer):
+    labels = serializers.StringRelatedField(many=True)
     class Meta:
-        model = Note
-        fields = "__all__"
+        model            = Note
+        fields           = "__all__"
         read_only_fields = ['id', 'user']
 
   
