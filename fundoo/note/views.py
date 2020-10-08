26cf02a8536f666a9f78075ef115970ed3aa5cfb
deep-sarkar <deepsarkar96@gmail.com>
2020-10-08 21:39:11 +0530

    [Deep]Add.added get note serializer for GetNote view

--- fundoo/note/views.py
+++ fundoo/note/views.py
@@ -1,13 +1,13 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from note.models import Note
-from note.serializers import NoteSerializer, EditNoteSerializer
+from note.models import Note, Label
+from note.serializers import NoteSerializer, EditNoteSerializer, GetNoteSerializer
 from status import response_code
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from note.exceptions import ObjectDoesNotExixts
-from django.db import connections
+from note.exceptions import RequestObjectDoesNotExixts
+from django.core.exceptions import ObjectDoesNotExist
 
 class CreateNote(GenericAPIView):
     serializer_class = NoteSerializer
@@ -16,17 +16,18 @@ class CreateNote(GenericAPIView):
         try:
             if not request.user.is_authenticated:
                 return Response({"code":413, "msg":response_code[413]})
-            serializer = NoteSerializer(data = request.data)
+            data = request.data
+            serializer = NoteSerializer(data = data)
             if serializer.is_valid():
                 serializer.save(user = request.user)
                 return Response({"code":201, "msg":response_code[201]})
             return Response({"code":300, "msg":response_code[300]})
-        except Exception:
+        except Exception as e:
             return Response({"code":416, "msg":response_code[416]})
 
 
 class GetNote(GenericAPIView):
-    serializer_class = NoteSerializer
+    serializer_class = GetNoteSerializer
 
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
@@ -37,7 +38,7 @@ class GetNote(GenericAPIView):
                        WHERE (trash = false) and (archive = false) and (user_id=%s) 
                        ORDER BY pin desc, id desc''' % user_id
             notes = Note.objects.raw(query)
-            serializer = NoteSerializer(notes, many=True)
+            serializer = GetNoteSerializer(notes, many=True)
             return Response({"data":serializer.data, "code":200, "msg":response_code[200]})
         except Exception:
             return Response({"code":416, "msg":response_code[416]})
@@ -50,21 +51,20 @@ class EditNote(GenericAPIView):
     def get_object(self,id):
         try:
             user_id = self.request.user.id
-            # note = Note.objects.filter(Q(user=user) & Q(trash=False))
             query = '''SELECT * 
                         FROM note_note 
-                        WHERE id=%s''' % id
+                        WHERE (id=%s) and (user_id=%s)''' % (id, user_id) 
             note = Note.objects.raw(query)
             return note[0]
         except IndexError:
-           raise ObjectDoesNotExixts(code=409, msg=response_code[409])
+           raise RequestObjectDoesNotExixts(code=409, msg=response_code[409])
 
     @method_decorator(login_required)
     def get(self, request, id=None):
         try:
             try:
                 note       = self.get_object(id)
-            except ObjectDoesNotExixts as e:
+            except RequestObjectDoesNotExixts as e:
                 return Response({'code':e.code, 'msg':e.msg})
             serializer = EditNoteSerializer(note)
             return Response({"data":serializer.data,"code":200, "msg":response_code[200]})
@@ -76,7 +76,7 @@ class EditNote(GenericAPIView):
         try:
             try:
                 note       = self.get_object(id)
-            except ObjectDoesNotExixts as e:
+            except RequestObjectDoesNotExixts as e:
                 return Response({'code':e.code, 'msg':e.msg})
             serializer = EditNoteSerializer(note, data=request.data, partial=True)
             if serializer.is_valid():
