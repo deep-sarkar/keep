4f37415a846dcbea7342b42247b0c7c9ac12f30f
deep-sarkar <deepsarkar96@gmail.com>
2020-10-09 11:26:50 +0530

    [Deep]Add.added delete label endpoint

--- fundoo/label/urls.py
+++ fundoo/label/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from label.views import GetLabel, CreateLabel, EditLabel
+from label.views import GetLabel, CreateLabel, EditLabel, DeleteLabel
 
 urlpatterns = [
     path('create/', CreateLabel.as_view()),
     path('get/', GetLabel.as_view()),
     path('edit/<int:id>/', EditLabel.as_view()),
+    path('delete/<int:id>/', DeleteLabel.as_view()),
 ]
