0cd80911d5cac618c2bb9d49f024faa99ac3d114
deep-sarkar <deepsarkar96@gmail.com>
2020-10-09 10:21:21 +0530

    [Deep]Add.added archive note endpoint

--- fundoo/note/urls.py
+++ fundoo/note/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from note.views import CreateNote, GetNote, EditNote, TrashNote, CreateLabel
+from note.views import ( 
+                        CreateNote, 
+                        GetNote, 
+                        EditNote, 
+                        TrashNote, 
+                        CreateLabel,
+                        ArchiveNote
+                        )
 
 urlpatterns = [
     path('create/', CreateNote.as_view()),
     path('get/', GetNote.as_view()),
     path('edit/<int:id>/', EditNote.as_view()),
     path('trash/', TrashNote.as_view()),
-    path('label/', CreateLabel.as_view())
+    path('label/', CreateLabel.as_view()),
+    path('archive/', ArchiveNote.as_view()),
 ]
