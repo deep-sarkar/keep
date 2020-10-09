ef765b8276ed56da931345451908f019559e84f5
deep-sarkar <deepsarkar96@gmail.com>
2020-10-09 09:39:03 +0530

    [Deep]Add.added add label functions

--- fundoo/note/urls.py
+++ fundoo/note/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from note.views import CreateNote, GetNote, EditNote, TrashNote
+from note.views import CreateNote, GetNote, EditNote, TrashNote, CreateLabel
 
 urlpatterns = [
     path('create/', CreateNote.as_view()),
     path('get/', GetNote.as_view()),
     path('edit/<int:id>/', EditNote.as_view()),
-    path('trash/', TrashNote.as_view())
+    path('trash/', TrashNote.as_view()),
+    path('label/', CreateLabel.as_view())
 ]
