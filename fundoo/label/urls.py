d57971f6756f0a590cd26bdfa07f00069c7c949c
deep-sarkar <deepsarkar96@gmail.com>
2020-10-09 11:02:52 +0530

    [Deep]Add.added edit label endpoint

--- fundoo/label/urls.py
+++ fundoo/label/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from label.views import GetLabel, CreateLabel
+from label.views import GetLabel, CreateLabel, EditLabel
 
 urlpatterns = [
     path('create/', CreateLabel.as_view()),
     path('get/', GetLabel.as_view()),
+    path('edit/<int:id>/', EditLabel.as_view()),
 ]
