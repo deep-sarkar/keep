69132b35af119414cbb92052465a48a7e10bdf02
deep-sarkar <deepsarkar96@gmail.com>
2020-10-10 10:11:19 +0530

    [Deep]Add.added registered UserMap model into admin site

--- fundoo/note/admin.py
+++ fundoo/note/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Note, LabelMap
+from .models import Note, LabelMap, UserMap
 
 admin.site.register(Note)
 admin.site.register(LabelMap)
+admin.site.register(UserMap)
