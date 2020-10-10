f335406b381f65dc9658d4e8b51799686b4ff86f
deep-sarkar <deepsarkar96@gmail.com>
2020-10-10 10:37:32 +0530

    [Deep]Add.added status for collaborator mapping error

--- fundoo/util/status.py
+++ fundoo/util/status.py
@@ -28,5 +28,6 @@ response_code = {
     414 : "Invalid operation",
     415 : "Enter upcoming time",
     416 : "Something went wrong. Pleaase Try again",
-    417 : "Error occoured while mapping label"
+    417 : "Error occoured while mapping label",
+    418 : "Error occoured while mapping collaborators"
 }
