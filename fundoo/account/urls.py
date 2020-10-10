4aff67ae77234e19bbe292adb5600c97e117f8df
deep-sarkar <deepsarkar96@gmail.com>
2020-10-10 15:22:26 +0530

    [Deep]Refactor.removed user existance url

--- fundoo/account/urls.py
+++ fundoo/account/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from .views import(Registration, 
                     LoginAPIView, 
                     Logout, 
-                    ResetPasswordView, 
+                    ChangePasswordView, 
                     ActivateAccount, 
                     ForgotPasswordView,
-                    CheckUserExistance,
                     ResetNewPassword
                     ) 
 from rest_framework_jwt.views import refresh_jwt_token
@@ -15,10 +14,9 @@ urlpatterns = [
     path('register/', Registration.as_view(), name='registration'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
-    path('change_password/',ResetPasswordView.as_view(), name='change_password'),
+    path('change_password/',ChangePasswordView.as_view(), name='change_password'),
     path('activate/<surl>/', ActivateAccount.as_view(), name='activate'),
     path('forgot_password/',ForgotPasswordView.as_view(), name='forgot_password'),
-    path('check_user/<surl>/',CheckUserExistance.as_view(), name='checkUserExistance'),
-    path('reset_paassword/', ResetNewPassword.as_view(), name='reset_new_password'),
+    path('reset_password/<str:surl>/', ResetNewPassword.as_view(), name='reset_new_password'),
     path('token_refresh/', refresh_jwt_token)
 ]
