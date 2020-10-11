d625e49d58433fbf93408ae3aa057af8a9d84868
deep-sarkar <deepsarkar96@gmail.com>
2020-10-11 21:10:20 +0530

    [Deep]Add.added expiry date to token

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -161,8 +161,8 @@ JWT_AUTH = {
     'JWT_PAYLOAD_HANDLER':
     'rest_framework_jwt.utils.jwt_payload_handler',
     
-    # 'JWT_ALLOW_REFRESH': True,
-    # 'JWT_EXPIRATION_DELTA': datetime.timedelta(days=3),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=3),
 }
 
 
