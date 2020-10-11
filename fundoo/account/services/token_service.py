51cc4af15babb839fbb7e74fff83ee51a62d88be
deep-sarkar <deepsarkar96@gmail.com>
2020-10-11 21:10:04 +0530

    [Deep]Add.added login token service

--- fundoo/account/services/token_service.py
+++ fundoo/account/services/token_service.py
@@ -1,7 +1,24 @@
 import jwt
+from account import redis
+from rest_framework_jwt.settings import api_settings
+
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 
 
 def generate_token(payload):
     key = "secret"
     token = jwt.encode(payload, key).decode('utf-8')
+    return token
+
+def generate_login_token(user):
+    username = user.username
+    try:
+        token = redis.get_attribute(username)
+        jwt.decode(token)
+    except Exception as e:
+        pass
+    if token == None:
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
     return token
