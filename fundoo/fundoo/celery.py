dc675653fb23316bd452475b70e011bc93e412ff
deep-sarkar <deepsarkar96@gmail.com>
2020-10-11 19:41:57 +0530

    [Deep]Add.added send notification as periodic task

--- fundoo/fundoo/celery.py
+++ fundoo/fundoo/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fundoo.settings')
@@ -16,6 +17,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    "every-1-hour":{
+        "task":"note.task.send_reminder_notification",
+        "schedule":3600
+    }
+}
 
 @app.task(bind=True)
 def debug_task(self):
