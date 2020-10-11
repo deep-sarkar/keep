26af47e8a1dd292971e17eb5519f36d42647b9b6
deep-sarkar <deepsarkar96@gmail.com>
2020-10-11 17:56:26 +0530

    [Deep]Add.added reminder mail subject to static data

--- fundoo/util/static_data.py
+++ fundoo/util/static_data.py
@@ -1,3 +1,5 @@
 ITEMS_PER_PAGE = 3
 ACCOUNT_ACTIVATION_SUBJECT = "Account activation url"
 PASSWORD_RESET_MESSAGE = "Reset Your password by clicking below link"
+REMINDER_SUB = "A reminder is set in fundoo notes."
+
