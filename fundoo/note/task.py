bb9a7a63ce5a66b9191766f948c0720b52c2c7b7
deep-sarkar <deepsarkar96@gmail.com>
2020-10-11 19:42:49 +0530

    [Deep]Add.added send reminder notification task

--- fundoo/note/task.py
+++ fundoo/note/task.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from fundoo.settings import EMAIL_HOST_USER
 from util import static_data
+from services.reminder_service import get_note_reminders
 
 
 @shared_task
@@ -10,3 +11,14 @@ def send_reminder_mail(rem,recipient):
     msg = "You have a reminder in fundoo at {}".format(rem)
     send_mail(sub, msg, EMAIL_HOST_USER, [recipient])
     return None
+
+@shared_task
+def send_reminder_notification():
+    reminders = get_note_reminders()
+    if len(reminders) != 0:
+        for reminder in reminders:
+            sub = static_data.REMINDER_SUB
+            msg = "You have a reminder in fundoo at {}".format(reminder[0])
+            send_mail(sub, msg, EMAIL_HOST_USER, [reminder[1]])
+    return None
+
