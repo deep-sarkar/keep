d20b0a3ff4f6e72fbbdf8212d6d777a6f5fd0101
deep-sarkar <deepsarkar96@gmail.com>
2020-10-12 12:57:38 +0530

    [Deep]Add.added set redis as default cache backend

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -155,6 +155,15 @@ REST_FRAMEWORK = {
     ),
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+    }
+}
 
 CELERY_BROKER_URL = 'redis://localhost:6379/4'
 CELERY_TASK_SERIALIZER = 'json'
