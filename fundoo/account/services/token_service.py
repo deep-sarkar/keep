bbeaad7240c1972b74054310a592e825e8927f16
deep-sarkar <deepsarkar96@gmail.com>
2020-10-12 08:59:09 +0530

    [Deep]Add.added expiry times in token

--- fundoo/account/services/token_service.py
+++ fundoo/account/services/token_service.py
@@ -1,24 +1,25 @@
 import jwt
 from account import redis
-from rest_framework_jwt.settings import api_settings
+import datetime
 
-jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 
 
 def generate_token(payload):
     key = "secret"
+    payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(days=1)
     token = jwt.encode(payload, key).decode('utf-8')
     return token
 
-def generate_login_token(user):
-    username = user.username
+def generate_login_token(payload):
+    key = "secret"
+    username = payload['username']
     try:
         token = redis.get_attribute(username)
-        jwt.decode(token)
+        jwt.decode(token,'secret')
     except Exception as e:
         pass
     if token == None:
-        payload = jwt_payload_handler(user)
-        token = jwt_encode_handler(payload)
+        payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(days=3)
+        token = jwt.encode(payload, key).decode('utf-8')
+        redis.set_attribute(username,token)
     return token
