f322a9b92189cccf0b07f1e2081f602dcc179ffe
deep-sarkar <deepsarkar96@gmail.com>
2020-10-12 09:34:14 +0530

    [Deep]Add.added refresh token function

--- fundoo/account/services/token_service.py
+++ fundoo/account/services/token_service.py
@@ -1,6 +1,7 @@
 import jwt
 from account import redis
 import datetime
+from util.status import response_code
 
 
 
@@ -23,3 +24,20 @@ def generate_login_token(payload):
         token = jwt.encode(payload, key).decode('utf-8')
         redis.set_attribute(username,token)
     return token
+
+def refresh_token(token):
+    key = "secret"
+    try:
+        decode = jwt.decode(token, 'secret')
+    except jwt.DecodeError:
+        return {"code":306,"msg":response_code(306)}
+    except jwt.ExpiredSignatureError:
+        return {"code":304,"msg":response_code(304)}
+    username = decode.get('username')
+    payload = {"username":username}
+    
+    payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    new_token = jwt.encode(payload, key).decode('utf-8')
+    print(new_token)
+    redis.set_attribute(username,new_token)
+    return {"token":new_token, "code":200, "msg":response_code[200]}
