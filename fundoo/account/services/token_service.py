6d0bff1daeb31c72eac3a8d41852479c6500a5d4
deep-sarkar <deepsarkar96@gmail.com>
2020-10-13 09:41:43 +0530

    [Deep]Refactor.modified token key name

--- fundoo/account/services/token_service.py
+++ fundoo/account/services/token_service.py
@@ -15,14 +15,17 @@ def generate_login_token(payload):
     key = "secret"
     username = payload['username']
     try:
-        token = redis.get_attribute(username)
+        token_key = username +'_token_key'
+        token = redis.get_attribute(token_key)
         jwt.decode(token,'secret')
     except Exception as e:
         pass
     if token == None:
         payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(days=3)
         token = jwt.encode(payload, key).decode('utf-8')
-        redis.set_attribute(username,token)
+        token_key = username +'_token_key'
+        print(token_key)
+        redis.set_attribute(token_key,token)
     return token
 
 def refresh_token(token):
@@ -38,5 +41,6 @@ def refresh_token(token):
     
     payload["exp"] = datetime.datetime.utcnow() + datetime.timedelta(days=1)
     new_token = jwt.encode(payload, key).decode('utf-8')
-    redis.set_attribute(username,new_token)
+    token_key = username +'_token_key'
+    redis.set_attribute(token_key,new_token)
     return {"token":new_token, "code":200, "msg":response_code[200]}
