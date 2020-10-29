57bfb26834fd728dd810ab83073f840dbbf5965c
deep-sarkar <deepsarkar96@gmail.com>
2020-10-29 12:55:21 +0530

    [Deep]Add.added function to save image

--- fundoo/note/request_data_handler.py
+++ fundoo/note/request_data_handler.py
@@ -1,3 +1,7 @@
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+
 def fetch_collaborator(request):
     try:
         collaborators = request.data.get('collaborators')
@@ -17,4 +21,16 @@ def fetch_reminder(request):
         rem = request.data.get('reminder')
         return rem
     except KeyError:
+        return None
+
+def save_image(request):
+    try:
+        image = request.FILES['image']
+        name = str(request.user.id) + image.name 
+        with default_storage.open(name , 'wb+') as destination:
+            for chunk in image.chunks():
+                destination.write(chunk)
+        request.data['image'] = name
+        return name
+    except Exception as e:
         return None
